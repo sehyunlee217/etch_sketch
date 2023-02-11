2dc64b4ee5dd9ac23c9bc48fbd34b39c0cc3078f
sehyun <sehyun.lee217@gmail.com>
2023-02-12 00:59:35 +0900

    Create a delete button for drawn elements Create a rainbow button that changes color of drawn elements Clean up css by adjusting the borders

--- eas.js
+++ eas.js
@@ -1,46 +1,100 @@
+// 2023.Feb.12 
+// Basic functionalities are completed for the Etch-a-Sketch
+// Rainbow button is not functioning properly, but will update buttonRainbow() in the future.
+
+// get random rgb 
+function randomRGB() {
+    // rgb is created by rgb(#,#,#); where # is between 1-256
+    let r = Math.random() * (256);
+    let g = Math.random() * (256);
+    let b = Math.random() * (256);
+    let rgb = `rgb(${ r },${ g },${ b })`;
+    return rgb;
+}
+
 function changeColor(e) {
-    // add property that would 
-    console.log(e);
+    // add property that would change color if mouse is hovered
     this.classList.add("hovered");
 };
 
-// create 16x rows for grid
+// change the background of hovered squares to rainbow
+function setHovertoRainbow(e) {
+    const sqrs = document.querySelectorAll(".hovered");
+    sqrs.forEach(sqr => sqr.style.backgroundColor = randomRGB());
+};
+// .style.backgroundColor = randomRGB();
+
+// create hover effect
+function hover() {
+    // when the div class="square" is hovered, change color 
+    const sqrs = document.querySelectorAll(".square");
+    sqrs.forEach(sqr => sqr.addEventListener("mouseover", changeColor));
+}
+
+// create the entire grid depending on the number of squares for each row
 function createGrid(sqrsEachrow) {
+    // create rows
     for (let i = 0; i < sqrsEachrow; i++) {
-        // create row element, with class "row{row_number}"
+        // create row element by adding class="row" 
         const row = document.createElement("div");
         row.classList.add("row");
+        // create squares for the rows
         for (let j = 0; j < sqrsEachrow; j++) {
-            // select row and add 16x squares to the row
+            // for each row, attatch square elements to the row
             const square = document.createElement('div');
             square.classList.add("square");
-            // create hover effect
-            // when the div class="square" is hovered, change color 
-            square.addEventListener("mouseover", changeColor);
             row.appendChild(square);
         }
         const grid = document.querySelector(".grid");
+        // add each row to the entire grid
         grid.appendChild(row);
     }
+    hover();
 };
 
 // function that removes current grid
+// prevents new grids merging with previous grids
 function removeGrid() {
+    // remove all elements from the grid
     const rows = document.querySelectorAll(".row");
-    console.log(rows);
     rows.forEach(row => row.remove());
 };
 
+// promts message when clicked the "Number of squares per side" button
 function promptMsg(e) {
-    let numSqrs = window.prompt("Enter number of squares per side", 16);
-    if (numSqrs <= 100) {
+    // prompt message, default 30
+    let numSqrs = window.prompt("Enter number of squares per side", 30);
+    // As long as the number of squares per side are equal or smaller to 64
+    if (numSqrs <= 64) {
+        // remove grid to prevent merging with previous grids
         removeGrid();
+        // create grid based off number of squares prompted
         createGrid(numSqrs);
     }
 }
 
+// erases all etch marks by removing the "hovered" attribute
+function eraseHover(e) {
+    const sqrs = document.querySelectorAll(".square");
+    sqrs.forEach(sqr => sqr.classList.remove("hovered"));
+}
+
+// Start of Script -------------------------------
+
+createGrid(30);
+
 // when button is clicked, change number of squares in each row
-const buttonPromt = document.querySelector("button");
+const buttonPromt = document.querySelector(".prompt");
 buttonPromt.addEventListener("click", promptMsg);
+
+// when Erase button is clicked, erase all hovered squares
+const buttonErase = document.querySelector(".erase");
+buttonErase.addEventListener("click", eraseHover);
+
+// when Rainbow button is clicked, change color to randomRGB();
+const buttonRainbow = document.querySelector(".rainbow");
+buttonRainbow.addEventListener("click", setHovertoRainbow);
+
+
 
 
